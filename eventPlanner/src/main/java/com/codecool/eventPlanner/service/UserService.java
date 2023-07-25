c1dbcacf4e000e3458567b3a7614b580cb796cb3
matheanna <113627409+matheanna@users.noreply.github.com>
2023-07-25 13:42:24 +0200

    feat: implement handling data

--- eventPlanner/src/main/java/com/codecool/eventPlanner/service/UserService.java
+++ eventPlanner/src/main/java/com/codecool/eventPlanner/service/UserService.java
@@ -1,5 +1,19 @@
 package com.codecool.eventPlanner.service;
 
 
+import com.codecool.eventPlanner.model.NewUserDTO;
+import com.codecool.eventPlanner.model.UserDTO;
+
+import java.util.List;
+
 public interface UserService {
+    boolean updateUser(UserDTO userDTO);
+
+    UserDTO getUserById(int id);
+
+    List<UserDTO> getAll();
+
+    boolean delete(int id);
+
+    boolean createUser(NewUserDTO userDTO);
 }
