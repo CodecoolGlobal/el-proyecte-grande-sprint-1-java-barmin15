955c2134310da80b98c3f9fe8a1dc16bbf6bf82a
László Szakszon <115426308+szaxon@users.noreply.github.com>
2023-07-25 14:08:55 +0200

    feat: create endpoints in 'EventController'

--- eventPlanner/src/main/java/com/codecool/eventPlanner/controller/EventController.java
+++ eventPlanner/src/main/java/com/codecool/eventPlanner/controller/EventController.java
@@ -1,9 +1,14 @@
 package com.codecool.eventPlanner.controller;
 
+import com.codecool.eventPlanner.model.EventDTO;
+import com.codecool.eventPlanner.model.NewEventDTO;
+import com.codecool.eventPlanner.model.UpdateEventDTO;
 import com.codecool.eventPlanner.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/event")
@@ -14,4 +19,36 @@ public class EventController {
     public EventController(EventService eventService){
         this.eventService = eventService;
     }
+
+    @GetMapping("/all")
+    public List<EventDTO> getAllEvents(){
+        return eventService.getAllEvents();
+    }
+
+    @GetMapping("/{id}")
+    public EventDTO getEventById(@PathVariable int id){
+        return eventService.getEventById(id);
+    }
+
+    @GetMapping("/category")
+    public List<EventDTO> getEventsByCategories(@RequestBody List<Integer> categoryIds) {
+        return eventService.getEventsByCategories(categoryIds);
+    }
+
+    @PostMapping()
+    public boolean createEvent(@RequestBody NewEventDTO newEventDTO) {
+        return eventService.createEvent(newEventDTO);
+    }
+
+    @PutMapping
+    public boolean updateEvent(@RequestBody UpdateEventDTO updateEventDTO) {
+        return eventService.updateEvent(updateEventDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteEvent(@PathVariable int id) {
+        return eventService.deleteEvent(id);
+    }
+
+
 }
