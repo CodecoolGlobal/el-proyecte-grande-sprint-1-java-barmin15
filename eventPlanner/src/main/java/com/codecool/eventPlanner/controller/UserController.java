edb1d49aff0c7c1feb48816ea6772970be5c3328
matheanna <113627409+matheanna@users.noreply.github.com>
2023-07-25 13:41:23 +0200

    feat: create endpoints in 'UserController'

--- eventPlanner/src/main/java/com/codecool/eventPlanner/controller/UserController.java
+++ eventPlanner/src/main/java/com/codecool/eventPlanner/controller/UserController.java
@@ -1,18 +1,52 @@
 package com.codecool.eventPlanner.controller;
 
+import com.codecool.eventPlanner.model.NewUserDTO;
+import com.codecool.eventPlanner.model.UserDTO;
 import com.codecool.eventPlanner.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @GetMapping("/getById")
+    public UserDTO getUserById(@RequestBody int id){
+        return userService.getUserById(id);
+    }
+
+    @GetMapping("/all")
+    public List<UserDTO> getAllUsers(){
+        return userService.getAll();
+    }
+    @PostMapping()
+    public boolean createUser(@RequestBody NewUserDTO userDTO){
+        return userService.createUser(userDTO);
+    }
+
+    @PutMapping("/update")
+    public boolean updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
+    }
+    @DeleteMapping()
+    public boolean deleteUser(@RequestBody int id){
+        return userService.delete(id);
+    }
 }
+
+
+
+
+
+
+
+
