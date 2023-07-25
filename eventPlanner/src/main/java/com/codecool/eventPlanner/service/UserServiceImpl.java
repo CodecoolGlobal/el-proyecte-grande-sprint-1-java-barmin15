5854cbc0c107f3bf59c3fc5a9805827c54d2e99e
László Szakszon <115426308+szaxon@users.noreply.github.com>
2023-07-25 15:40:27 +0200

    feat: create methods in userImpl

--- eventPlanner/src/main/java/com/codecool/eventPlanner/service/UserServiceImpl.java
+++ eventPlanner/src/main/java/com/codecool/eventPlanner/service/UserServiceImpl.java
@@ -4,32 +4,51 @@ import com.codecool.eventPlanner.model.NewUserDTO;
 import com.codecool.eventPlanner.model.UserDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private int idCounter = 0;
+
+    private List<UserDTO> allUsers = new ArrayList<>();
+
+
     @Override
     public boolean updateUser(UserDTO userDTO) {
+        if(allUsers.contains(userDTO)){
+            UserDTO user = allUsers.stream().filter(u -> u.id() == userDTO.id()).findFirst().get();
+            allUsers.remove(user);
+            allUsers.add(userDTO);
+        }
         return false;
     }
 
     @Override
     public UserDTO getUserById(int id) {
-        return null;
+        return allUsers.stream().filter(user -> user.id() == id).findFirst().orElseGet(()->null);
     }
 
     @Override
     public List<UserDTO> getAll() {
-        return null;
+        return allUsers;
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+        UserDTO user = allUsers.stream().filter(u -> u.id() == id).findFirst().orElseGet(()->null);
+        return allUsers.remove(user);
     }
 
     @Override
     public boolean createUser(NewUserDTO userDTO) {
-        return false;
+        String name = userDTO.username();
+        String pw = userDTO.password();
+        allUsers.add(new UserDTO(idCounter, name, pw));
+        idCounter++;
+        return true;
     }
+
+
+
 }
