0a444dca4de4f2c427e01548c704424822c0a6c6
László Szakszon <115426308+szaxon@users.noreply.github.com>
2023-07-25 14:11:44 +0200

    feat: create methods for eventService

--- eventPlanner/src/main/java/com/codecool/eventPlanner/service/EventService.java
+++ eventPlanner/src/main/java/com/codecool/eventPlanner/service/EventService.java
@@ -1,7 +1,23 @@
 package com.codecool.eventPlanner.service;
 
 import com.codecool.eventPlanner.model.EventDTO;
+import com.codecool.eventPlanner.model.NewEventDTO;
+import com.codecool.eventPlanner.model.UpdateEventDTO;
+
+import java.util.List;
 
 public interface EventService {
     public EventDTO getEvent(int id);
+
+    List<EventDTO> getAllEvents();
+
+    EventDTO getEventById(int id);
+
+    List<EventDTO> getEventsByCategories(List<Integer> categoryIds);
+
+    boolean createEvent(NewEventDTO newEventDTO);
+
+    boolean deleteEvent(int id);
+
+    boolean updateEvent(UpdateEventDTO updateEventDTO);
 }
