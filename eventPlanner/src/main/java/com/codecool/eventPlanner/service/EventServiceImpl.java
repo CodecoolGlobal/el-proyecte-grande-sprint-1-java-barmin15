45f45dee6b0638cb7602120abea5226905d1515d
László Szakszon <115426308+szaxon@users.noreply.github.com>
2023-07-25 14:11:14 +0200

    feat: override methods for eventService

--- eventPlanner/src/main/java/com/codecool/eventPlanner/service/EventServiceImpl.java
+++ eventPlanner/src/main/java/com/codecool/eventPlanner/service/EventServiceImpl.java
@@ -1,12 +1,46 @@
 package com.codecool.eventPlanner.service;
 
 import com.codecool.eventPlanner.model.EventDTO;
+import com.codecool.eventPlanner.model.NewEventDTO;
+import com.codecool.eventPlanner.model.UpdateEventDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EventServiceImpl implements EventService{
     @Override
     public EventDTO getEvent(int id) {
         return null;
+    }
+
+    @Override
+    public List<EventDTO> getAllEvents() {
+        return null;
+    }
+
+    @Override
+    public EventDTO getEventById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<EventDTO> getEventsByCategories(List<Integer> categoryIds) {
+        return null;
+    }
+
+    @Override
+    public boolean createEvent(NewEventDTO newEventDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteEvent(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean updateEvent(UpdateEventDTO updateEventDTO) {
+        return false;
     }
 }
