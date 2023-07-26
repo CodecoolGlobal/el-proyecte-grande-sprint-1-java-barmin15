5e742a9d81b0253b4c7672d0bd7cf1543f9b55b4
matheanna <113627409+matheanna@users.noreply.github.com>
2023-07-26 18:31:57 +0200

    fix: fix filepath

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./Vitals/reportWebVitals";
 import Layout from "./Layout/layout";
 import App from "./App/App";
 import EventForm from "./components/EventForm";
-import AllEvent from "./components/AllEvent";
+import AllEvent from "./components/AllEvent.js";
 
 const Router = createBrowserRouter([
   {
