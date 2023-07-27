63d655fb02aed0ef768582e42af0c62ba3b9f4e4
barmin15 <115711430+barmin15@users.noreply.github.com>
2023-07-27 09:58:25 +0200

    feat: add side navigation button routing

--- front-end/src/Layout/layout.jsx
+++ front-end/src/Layout/layout.jsx
@@ -8,8 +8,8 @@ function Layout(){
     <div href="" id="logo">EVENET</div>
   </div>
   <div id="mySidenav" className="sidenav">
-  <a href="/" id="addEvent">Add</a>
-  <a href="/" id="allEvents">All</a>
+  <a href="/create" id="addEvent">Add</a>
+  <a href="/events" id="allEvents">All</a>
 </div>
    <Outlet/>
   </div>
