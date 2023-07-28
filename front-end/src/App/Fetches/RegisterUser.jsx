fb8f922fc157b6ffed9ae6ebe5b552830adec43d
matheanna <113627409+matheanna@users.noreply.github.com>
2023-07-28 13:13:53 +0200

    fix: register and login fetch fixed

---
+++ front-end/src/App/Fetches/RegisterUser.jsx
@@ -0,0 +1,20 @@
+async function RegisterUser(username, password) {
+
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password })
+};
+ return fetch('http://localhost:0420/user', requestOptions)
+    .then(response => response.json())
+    .then((responseData) => {
+      if (responseData === true) {
+        return true;
+      }
+      return false;
+    });
+
+}
+
+
+export { RegisterUser };
---
+++ front-end/src/App/Pages/RegisterPage.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useAsyncError, useNavigate } from "react-router-dom";
+import "./LogIn.css"
+import { RegisterUser } from "../Fetches/RegisterUser";
+
+export default function RegisterPage() {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    
+    async function register() {
+        if (username.length > 5 && password.length > 5) {
+            if (await RegisterUser(username, password)) {
+                navigate("/event/all")
+            }
+        }
+    }
+
+
+    
+
+    return (<div className="container flex">
+        <div className="form">
+            <input type="" placeholder="Username" required onChange={(e) => setUsername(e.target.value)}></input>
+            <input type="password" placeholder="Password" required onChange={(e) => setPassword(e.target.value)}></input>
+            <div className="link">
+                <button  className="login" onClick={() => register()}>Register</button>
+            </div>
+            <div className="button">
+                <a href="/login">Already A User?</a>
+            </div>
+            </div>
+            </div>)
+}
