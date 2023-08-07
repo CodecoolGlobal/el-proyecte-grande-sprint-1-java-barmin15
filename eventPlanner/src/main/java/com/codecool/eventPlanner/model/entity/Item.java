e10db30f03a521c6c69376d4b4413005932d3f64
László Szakszon <115426308+szaxon@users.noreply.github.com>
2023-08-07 13:54:43 +0200

    create: new item table-entity

---
+++ eventPlanner/src/main/java/com/codecool/eventPlanner/model/entity/Item.java
@@ -0,0 +1,20 @@
+package com.codecool.eventPlanner.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
