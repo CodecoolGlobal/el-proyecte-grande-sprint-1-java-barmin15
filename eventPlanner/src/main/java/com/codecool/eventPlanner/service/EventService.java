ff8447bbba7dfc250ad7b08bdade80d01fb5ca3f
László Szakszon <115426308+szaxon@users.noreply.github.com>
2023-08-08 10:51:09 +0200

    build: create eventservice

--- eventPlanner/src/main/java/com/codecool/eventPlanner/service/EventService.java
+++ eventPlanner/src/main/java/com/codecool/eventPlanner/service/EventService.java
@@ -1,22 +1,18 @@
 package com.codecool.eventPlanner.service;
 
-import com.codecool.eventPlanner.model.dto.EventDTO;
-import com.codecool.eventPlanner.model.dto.NewEventDTO;
-import com.codecool.eventPlanner.model.dto.UpdateEventDTO;
+import com.codecool.eventPlanner.model.entity.Event;
+import com.codecool.eventPlanner.model.entity.User;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public interface EventService {
 
-    List<EventDTO> getAllEvents();
+    List<Event> getAllEvents();
 
-    EventDTO getEventById(int id);
+    void addUserForEvent(User user, Event event);
 
-    List<EventDTO> getEventsByCategories(List<Integer> categoryIds);
-
-    boolean createEvent(NewEventDTO newEventDTO);
-
-    boolean deleteEvent(int id);
-
-    boolean updateEvent(UpdateEventDTO updateEventDTO);
+    Event getEventById(Long id);
 }
