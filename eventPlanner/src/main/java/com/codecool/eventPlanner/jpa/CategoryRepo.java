acaf0352c0d17d29fe1706b3d61cf3f72c96b01e
László Szakszon <115426308+szaxon@users.noreply.github.com>
2023-08-08 09:34:11 +0200

    create: add check function for categroy

--- eventPlanner/src/main/java/com/codecool/eventPlanner/jpa/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.eventPlanner.jpa;
-
-import com.codecool.eventPlanner.model.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CategoryRepo extends JpaRepository<Category, Long> {
-
-}
---
+++ eventPlanner/src/main/java/com/codecool/eventPlanner/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.codecool.eventPlanner.repository;
+
+import com.codecool.eventPlanner.model.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    boolean existsByName(String name);
+
+    Category findByName(String name);
+}
