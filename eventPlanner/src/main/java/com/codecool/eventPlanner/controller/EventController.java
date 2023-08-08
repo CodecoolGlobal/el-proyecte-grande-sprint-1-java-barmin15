730693e5cf8783532d4ac6bd5a78b1420dee15bb
László Szakszon <115426308+szaxon@users.noreply.github.com>
2023-08-08 10:50:27 +0200

    build: create eventController

--- eventPlanner/src/main/java/com/codecool/eventPlanner/controller/EventController.java
+++ eventPlanner/src/main/java/com/codecool/eventPlanner/controller/EventController.java
@@ -1,13 +1,37 @@
 package com.codecool.eventPlanner.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.eventPlanner.model.entity.Event;
+import com.codecool.eventPlanner.model.entity.User;
+import com.codecool.eventPlanner.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/event")
 public class EventController {
 
+    private EventService eventService;
 
+    @Autowired
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
 
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return eventService.getAllEvents();
+    }
 
+    @PutMapping
+    public void addUserForEvent(@RequestBody User user, Event event) {
+        eventService.addUserForEvent(user, event);
+    }
+
+    @GetMapping("{id}")
+    public Event getEventById(@PathVariable Long id) {
+        return eventService.getEventById(id);
+    }
 }
