2607fee2714871ea5141029bc89e71f2f1c5033c
barmin15 <115711430+barmin15@users.noreply.github.com>
2023-08-08 15:23:30 +0200

    update: event service and implementation

--- eventPlanner/src/main/java/com/codecool/eventPlanner/service/EventServiceImpl.java
+++ eventPlanner/src/main/java/com/codecool/eventPlanner/service/EventServiceImpl.java
@@ -1,22 +1,29 @@
 package com.codecool.eventPlanner.service;
 
+import com.codecool.eventPlanner.model.dto.CategoryIdsDTO;
+import com.codecool.eventPlanner.model.dto.EventDTO;
+import com.codecool.eventPlanner.model.dto.NewEventDTO;
+import com.codecool.eventPlanner.model.entity.Category;
 import com.codecool.eventPlanner.model.entity.Event;
 import com.codecool.eventPlanner.model.entity.User;
+import com.codecool.eventPlanner.repository.CategoryRepository;
 import com.codecool.eventPlanner.repository.EventRepository;
 import com.codecool.eventPlanner.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class EventServiceImpl implements EventService {
+    private final CategoryRepository categoryRepository;
     private final EventRepository eventRepository;
     private final UserRepository userRepository;
 
     @Autowired
-    public EventServiceImpl(EventRepository eventRepository, UserRepository userRepository) {
+    public EventServiceImpl(CategoryRepository categoryRepository, EventRepository eventRepository, UserRepository userRepository) {
+        this.categoryRepository = categoryRepository;
         this.eventRepository = eventRepository;
         this.userRepository = userRepository;
     }
@@ -33,10 +40,50 @@ public class EventServiceImpl implements EventService {
         Set<User> interestedUsers = modifiedEvent.getInterestedUsers();
         interestedUsers.add(user);
         eventRepository.save(modifiedEvent);
-         }
+    }
+
+
+
+    @Override
+    public void deleteEvent(Long id) {
+        eventRepository.deleteById(id);
+    }
+
+   /* @Override
+    public List<Event> getEventsByCategories(CategoryIdsDTO categoryIdsDTO) {
+        Set<Category> categories = Arrays.stream(categoryIdsDTO.categoryIds())
+                .map(id -> categoryRepository.findById(id).get()).collect(Collectors.toSet());
+        return eventRepository.getEventsByCategories(categories);
+    }*/
+
 
     @Override
     public Event getEventById(Long id) {
         return eventRepository.findById(id).get();
+    }
+
+    @Override
+    public Event updateEvent(Long id, EventDTO eventDTO) {
+        Event event = eventRepository.findById(id).get();
+        event.update(eventDTO);
+        eventRepository.save(event);
+        return event;
+    }
+
+    @Override
+    public Event createEvent(NewEventDTO newEventDTO) {
+        Event event = new Event(newEventDTO);
+        eventRepository.save(event);
+        return event;
+    }
+
+    @Override
+    public List<Event> getEventsByUser(Long id) {
+       return (List<Event>) userRepository.findById(id).get().getInterestedEvents();
+    }
+
+    @Override
+    public List<Event> getCreatedEventsByUser(Long id) {
+        return (List<Event>) userRepository.findById(id).get().getCreatedEvents();
     }
 }
