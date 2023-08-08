e88e56f3fd6e82b6ba6fd73e9578f983214d29e4
László Szakszon <115426308+szaxon@users.noreply.github.com>
2023-08-08 10:50:42 +0200

    build: create eventRepository

--- eventPlanner/src/main/java/com/codecool/eventPlanner/jpa/EventRepo.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.eventPlanner.jpa;
-
-import com.codecool.eventPlanner.model.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepo extends JpaRepository<Event, Long> {
-}
