972e8648e1ee42779c705dfd5838e7234808172b
László Szakszon <115426308+szaxon@users.noreply.github.com>
2023-08-11 10:15:17 +0200

    refactor: fetch url after using proxy

--- front-end/src/App/Fetches/GetUserById.jsx
+++ front-end/src/App/Fetches/GetUserById.jsx
@@ -1,21 +1,16 @@
 import { useState, useEffect } from "react";
 function useUserById() {
-    const [users, setUsers] = useState();
+  const [users, setUsers] = useState();
 
-    useEffect(() => {
-        const dataFetch = async () => {
-            const data = await (
-                await fetch(
-                    "http://localhost:0420/user/getById"
-                )
-            ).json();
-            setUsers(data);
-        };
+  useEffect(() => {
+    const dataFetch = async () => {
+      const data = await (await fetch("/user/getById")).json();
+      setUsers(data);
+    };
 
-        dataFetch();
-    }, []);
-    return users;
+    dataFetch();
+  }, []);
+  return users;
 }
 
 export default useUserById;
-
