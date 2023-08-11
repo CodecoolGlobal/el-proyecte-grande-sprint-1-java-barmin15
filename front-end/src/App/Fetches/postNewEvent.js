8383132f1cb7fabf079ea9bb0003b617d07ee3be
László Szakszon <115426308+szaxon@users.noreply.github.com>
2023-08-11 10:10:58 +0200

    refactor: only auto format

--- front-end/src/App/Fetches/postNewEvent.js
+++ front-end/src/App/Fetches/postNewEvent.js
@@ -1,13 +1,10 @@
-function PostNewEvent(event) {
-    return fetch("/event", {
-    
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(event),
-    })
-      .then((res) => res.json());
-
+async function postNewEvent(event) {
+  return fetch("/event", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(event),
+  }).then((res) => res.json());
 }
-export {PostNewEvent};
+export { postNewEvent };
