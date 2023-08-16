9bd10f319eb58ee4ca4cf4a634f29af9abf3e617
matheanna <113627409+matheanna@users.noreply.github.com>
2023-08-16 09:04:01 +0200

    refactor: add classnames, ids, add item list

--- front-end/src/App/Pages/EventPage.jsx
+++ front-end/src/App/Pages/EventPage.jsx
@@ -35,20 +35,43 @@ function EventPage() {
       console.error("Error:", error);
     }
   }
-
+  event && console.log(event);
+  if(event){
+     event.items = [{name: "food"}, {name:"drink"}, {name:"sunscreen"}, {name:"boardgames"}, {name:"backpack"}]
+  }
+  
   return (
-    <div className="eventt">
-      {event && (
-        <div>
-          <div className="details">
-            
-          <h1>{event.title}</h1>
-          <h2>{event.dateTime}</h2>
-          <h2>{event.location}</h2>
+    <div className="Event">
+         <div className="items">
+          <h4>Necessary items:</h4>
+            {event && (
+              <ul>
+                {event.items.map((item, index) => (
+                  <li id={index}>{item.name}</li>
+                ))}
+              </ul>   
+            )}
           </div>
-          <p>{event.description}</p>
+      {event && (
+        <div className="eventt">
+
+          <div className="details">  
+           <h1 id="title">{event.title}</h1>
+           <div className="dateTime location">
+           <h2 id="dateTime">{event.dateTime.substring(0,18)}</h2>
+           <h2> | </h2>
+           <h2 id="location">{event.location}</h2>
+           </div>
+           <h2 id="category">{event.categories.length > 0 && event.categories[0].name}</h2>
+           <p id="description">{event.description}</p>
+           <button onClick={handleClick}>Save</button>
+          </div>
+
+          <div>
+           <div className="picture"> <img src="https://i.pinimg.com/564x/4b/50/9a/4b509acbc36049b967d12e705a7990a1.jpg" alt="event pic" height={486} width={369}/> </div>
+          </div>
+       
           
-            <button onClick={handleClick}>Apply</button>
           
         </div>
       )}
