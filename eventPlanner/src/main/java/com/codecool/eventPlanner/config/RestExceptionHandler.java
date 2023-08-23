24d4eb34dc7f45de697fe4083c2d39c4ce53ec51
barmin15 <115711430+barmin15@users.noreply.github.com>
2023-08-23 18:55:19 +0200

    build: configuration classes for security

---
+++ eventPlanner/src/main/java/com/codecool/eventPlanner/config/RestExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.codecool.eventPlanner.config;
+
+import com.codecool.eventPlanner.exceptions.AppException;
+import com.codecool.eventPlanner.model.dto.ErrorDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(value = {AppException.class})
+    @ResponseBody
+    public ResponseEntity<ErrorDto> handleException(AppException ex) {
+        return ResponseEntity.status(ex.getCode())
+                .body(ErrorDto.builder().message(ex.getMessage()).build());
+    }
+}
