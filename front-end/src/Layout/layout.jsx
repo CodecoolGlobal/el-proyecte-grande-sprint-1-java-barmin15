1f9cc148ce1affd08fc114ea43653580b12eea82
barmin15 <115711430+barmin15@users.noreply.github.com>
2023-08-24 14:50:33 +0200

    refactor: navigation instead of href

--- front-end/src/Layout/layout.jsx
+++ front-end/src/Layout/layout.jsx
@@ -1,16 +1,23 @@
 import { Outlet } from "react-router-dom"
 import "./layout.css"
+import { useNavigate } from "react-router-dom"
 
 function Layout(){
+const navigate = useNavigate();
+
+  function onAddEvent(){navigate("/event/create");}
+  function onAllEVents(){navigate("/event/all");}
+  function onLogOut(){navigate("/");}
+  function onProfile(){navigate("/event/ownprofile");}
     return <div>
     <div className="topnav">
-    <a className="active" href="/event/ownprofile">Profile</a>
+    <a className="active" onClick={onProfile}>Profile</a>
     <div href="" id="logo">EVENET</div>
   </div>
   <div id="mySidenav" className="sidenav">
-  <a href="/event/create" id="addEvent">Add</a>
-  <a href="/event/all" id="allEvents">All</a>
-  <a href="/" id="logOut">Log Out</a>
+  <h1 onClick={onAddEvent} id="addEvent">Add</h1>
+  <h1 onClick={onAllEVents} id="allEvents">All</h1>
+  <h1 onClick={onLogOut} id="logOut">Log Out</h1>
 </div>
    <Outlet/>
   </div>
