d3d4e85963c5c99ab7e61eda932eeced9ebbc91f
barmin15 <115711430+barmin15@users.noreply.github.com>
2023-08-24 12:51:42 +0200

    add: user controller current user

--- eventPlanner/src/main/java/com/codecool/eventPlanner/controller/UserController.java
+++ eventPlanner/src/main/java/com/codecool/eventPlanner/controller/UserController.java
@@ -3,6 +3,9 @@ package com.codecool.eventPlanner.controller;
 import com.codecool.eventPlanner.model.entity.User;
 import com.codecool.eventPlanner.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -36,7 +39,15 @@ public class UserController {
 
     @GetMapping("/current")
     public User getCurrentUser() {
-        return userService.getCurrentUser();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String auth = authentication.getName();
+            auth = auth.substring(auth.indexOf("=") + 1);
+            auth = auth.substring(0, auth.indexOf(","));
+            return userService.getUserById(Long.parseLong(auth));
+        } else {
+            throw new RuntimeException("No User");
+        }
     }
 
     @GetMapping("/interested/{eventId}")
